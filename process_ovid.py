ef2a40cadf10398e0483f7ea0cadd5438e2bbadd
darrenkjr <darren.rajit@gmail.com>
2024-12-03 14:00:56 +1100

    fixed bugged in pubmed 2nd round search matching

--- process_ovid.py
+++ process_ovid.py
@@ -19,17 +19,25 @@ ss_results = pd.read_excel(doi_pmid_results_path, sheet_name = 'api_results_ss')
 
 #consolidate all retrieved titles into 1 
 title_template_df = oa_results.copy()
+#set index of title_template_df 
+title_template_df.set_index('included_article_id', inplace = True)
+emb_results.set_index('included_article_id', inplace = True)
+pubmed_results.set_index('included_article_id', inplace = True)
+ss_results.set_index('included_article_id', inplace = True)
+
 title_template_df['title'] = title_template_df['title'].fillna(emb_results['title'])
 title_template_df['title'].fillna(pubmed_results['title'], inplace=True)
 title_template_df['title'].fillna(ss_results['title'], inplace=True)
 
+#set index of title_template_df 
+unsucessful_pubmed.set_index('included_article_id', inplace = True)
+unsucessful_embase.set_index('included_article_id', inplace = True)
 
-unsucessful_pubmed['title_matching'] = (
-    unsucessful_pubmed['title']
-    .fillna(unsucessful_pubmed['title_if_unavailable'])
-    .fillna(title_template_df['title'])
-)
+#empty title_matching column 
+unsucessful_pubmed['title_matching'] = unsucessful_pubmed['title_if_unavailable']
+unsucessful_pubmed['title_matching'].fillna(title_template_df['title'], inplace = True)
 
+#set index 
 unsucessful_embase['title_matching'] = (
     unsucessful_embase['title']
     .fillna(unsucessful_embase['title_if_unavailable'])
@@ -76,60 +84,67 @@ pubmed_second_results_df.rename(
 col_interest = ['api_id_retrieved', 'title_2ndsearch', 'abstract_2ndsearch', 'publication_year', 'venue', 'doi', 'authors']
 pubmed_second_results_df_interest = pubmed_second_results_df[col_interest]
 
+def find_match(row, result_df, result_col,  og_col): 
+    #reset_index result_df 
+    
+    
+    sim_score = result_df[result_col].apply(lambda x : calculate_similarity(row[og_col], x))
+    best_match_index = sim_score.idxmax() 
+    return pd.Series({
+    'best_match_index': best_match_index,
+    'similarity_score': sim_score.max(),
+    'best_match_string': row[og_col],
+    'matching_title_from_search_results': result_df.loc[best_match_index, result_col],
+    # 'included_article_id': row['included_article_id'],
+    'included_reference': row['included_reference']
+})    
+
 
 #perform matching - title on title 
 def compare_og_results(original_df, result_df): 
 
-    def find_match(row, result_df, result_col,  og_col): 
-        sim_score = result_df[result_col].apply(lambda x : calculate_similarity(row[og_col], x))
-        best_match_index = sim_score.idxmax() 
-        return pd.Series({
-        'best_match_index': best_match_index,
-        'similarity_score': sim_score.max(),
-        'best_match_string': row[og_col],
-        'matching_title_from_search_results': result_df.loc[best_match_index, result_col],
-        'included_article_id': row['included_article_id']
-    })
-
-    #find best matching results from second round search 
-    best_matches_df = original_df.apply(lambda row: find_match(row, result_df, 'title_2ndsearch', 'title_matching'), axis=1)
-    best_matches_df = best_matches_df.reset_index()
-    # Rename the 'index' column to 'original_index' for clarity
-    best_matches_df = best_matches_df.rename(columns={'index': 'original_index'})
-    best_matches_df = best_matches_df[best_matches_df['similarity_score'] >= 90]
-
-    #best match index corresponds with the index of result_df that needs to be merged with original_df  
-    result_df_best_match = result_df.loc[best_matches_df['best_match_index']]
-    best_match_df_indexed = best_matches_df.set_index('best_match_index')
-    result_df_best_match['matching_index'] = best_match_df_indexed['original_index']
-    result_df_best_match = result_df_best_match.set_index('matching_index')
-
-
-    result_df_best_match['search_round'] = 'round2'
-    original_df['search_round'] = 'round1'
-    #merge back with original_df 
-    updated_df = original_df.combine_first(result_df_best_match)
-
-    #drop duplicates baed on ibcluded_article_id 
-    updated_df.drop_duplicates(subset = 'included_article_id', keep = 'first', inplace = True)
-    updated_df_indexed = updated_df.set_index('included_article_id')
+    original_df = original_df.reset_index()
+    included_article_ids = original_df['included_article_id'].copy()
     
-    return updated_df_indexed
-
+    # Find matches using title similarity
+    best_matches_df = original_df.apply(
+        lambda row: find_match(row, result_df, 'title_2ndsearch', 'title_matching'), 
+        axis=1
+    )
+    
+    # Filter for good matches (similarity >= 90)
+    best_matches_df = best_matches_df[best_matches_df['similarity_score'] >= 90]
+    
+    # Get matching results from second round search based on the index at which the best match is found 
+    result_df_matched = result_df.loc[best_matches_df['best_match_index']].copy()
+    
+    # Set the correct index for merging
+    result_df_matched.index = best_matches_df.index
+    
+    # Add round indicators
+    result_df_matched['search_round'] = 'round2'
+    original_df['search_round'] = 'round1'
+    
+    # Set index back to included_article_id for final merge
+    original_df.set_index('included_article_id', inplace=True)
+    result_df_matched.index = included_article_ids[result_df_matched.index]
+    
+    # Combine and return
+    return original_df.combine_first(result_df_matched)
+    
 
 #takes in unsucessful search results and tries to add information from second round search 
-print('Processing embase second round results')
+# Process embase results
+print('processing embase results')
 unsuccessful_embase_updated = compare_og_results(unsucessful_embase, emb_second_results_df)
-og_embase_indexed = og_embase.set_index('included_article_id')
-og_embase_updated = og_embase_indexed.combine_first(unsuccessful_embase_updated)
-og_embase_updated.reset_index(inplace =True)
+og_embase_updated = og_embase.set_index('included_article_id').combine_first(unsuccessful_embase_updated)
+og_embase_updated.reset_index(inplace=True)
 
-print('Processing pubmed second round results')
+# Process pubmed results  
+print('processing pubmed results')
 unsuccessful_pubmed_updated = compare_og_results(unsucessful_pubmed, pubmed_second_results_df_interest)
-#merge back with og 
-og_pubmed_indexed = og_pubmed.set_index('included_article_id')
-og_pubmed_updated =og_pubmed_indexed.combine_first(unsuccessful_pubmed_updated)
-og_pubmed_updated.reset_index(inplace =True)
+og_pubmed_updated = og_pubmed.set_index('included_article_id').combine_first(unsuccessful_pubmed_updated)
+og_pubmed_updated.reset_index(inplace=True)
 
 file_path = 'retrieval_results/api_retrieved_pubmed_embase_2ndsearch.xlsx'
 with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
